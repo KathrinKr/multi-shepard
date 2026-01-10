aa93254c2767dc78bc843c02e8d776a5a7722cd3
Kati <kathrinkre@web.de>
2026-01-10 21:02:04 +0100

    changed voices setup

--- voices.js
+++ voices.js
@@ -1,52 +1,38 @@
 export const voices = [
   {
-    channel: 0,
-    octave: 0,
+    channel: 0, //front left
+    octave: [0, 7, 4],
+    waveform: "square", // 'sine', 'square', 'sawtooth', 'triangle'
+  },
+
+  {
+    channel: 1, //front right
+    octave: [2, 9, 6],
     waveform: "square", // 'sine', 'square', 'sawtooth', 'triangle'
   },
   {
-    channel: 1,
-    octave: 1,
+    channel: 2, //center
+    octave: [1, 8, 5],
     waveform: "square", // 'sine', 'square', 'sawtooth', 'triangle'
   },
   {
-    channel: 2,
-    octave: 2,
+    channel: 4, //surround left
+    octave: [6, 3, 0],
     waveform: "square", // 'sine', 'square', 'sawtooth', 'triangle'
   },
   {
-    channel: 3,
-    octave: 3,
+    channel: 5, //surround right
+    octave: [3, 0, 7],
     waveform: "square", // 'sine', 'square', 'sawtooth', 'triangle'
   },
   {
-    channel: 4,
-    octave: 4,
+    channel: 6, //rear left
+    octave: [5, 2, 4],
     waveform: "square", // 'sine', 'square', 'sawtooth', 'triangle'
   },
   {
-    channel: 5,
-    octave: 5,
-    waveform: "square", // 'sine', 'square', 'sawtooth', 'triangle'
-  },
-  {
-    channel: 6,
-    octave: 6,
-    waveform: "square", // 'sine', 'square', 'sawtooth', 'triangle'
-  },
-  {
-    channel: 7,
-    octave: 7,
-    waveform: "square", // 'sine', 'square', 'sawtooth', 'triangle'
-  },
-  {
-    channel: 8,
-    octave: 8,
-    waveform: "square", // 'sine', 'square', 'sawtooth', 'triangle'
-  },
-  {
-    channel: 9,
-    octave: 9,
+    channel: 7, //rear right
+    octave: [4, 1, 8],
     waveform: "square", // 'sine', 'square', 'sawtooth', 'triangle'
   },
 ];
